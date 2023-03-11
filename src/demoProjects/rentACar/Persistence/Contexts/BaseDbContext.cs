41250910350a95cac3a8635f60e7be4e2daac72e
yasarbudak <yasar.budak@hotmail.com>
2023-03-11 21:05:22 +0300

    Add new project

---
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    internal class BaseDbContext : DbContext
+    {
+      public DbSet<Brand> Brands { get; set; }
+    }
+}
