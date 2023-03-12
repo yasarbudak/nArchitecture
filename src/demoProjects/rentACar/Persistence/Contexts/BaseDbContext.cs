c830ae296838ad3ab68fc065c76949430a1d40c0
yasarbudak <yasar.budak@hotmail.com>
2023-03-12 14:34:06 +0300

    Add Brand entity in DbContext

--- src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -1,15 +1,36 @@
 ﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace Persistence.Contexts
 {
-    internal class BaseDbContext : DbContext
+  internal class BaseDbContext : DbContext
+  {
+    protected IConfiguration Configuration { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+
+    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
     {
-      public DbSet<Brand> Brands { get; set; }
+      Configuration = configuration;
     }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+    {
+      //if (!dbContextOptionsBuilder.IsConfigured)
+      //  base.OnConfiguring(dbContextOptionsBuilder.UseSqlServer(Configuration.GetConnectionString("SomeConnectionString")));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Brand>(brand =>
+      {
+        brand.ToTable("Brands").HasKey(k => k.Id);
+        brand.Property(p=>p.Id).HasColumnName("Id");
+        brand.Property(p=>p.Name).HasColumnName("Name");
+      });
+
+      Brand[] brandEntitySeeds = new Brand[] { new Brand(1, "BMW"), new Brand(2, "Mercedes") };
+      modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
+    }
+  }
 }
